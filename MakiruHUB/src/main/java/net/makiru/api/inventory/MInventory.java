35c0e1cb8d4bfe9a81d92a23b1265927a42eeeca
William J <foufou25795@hotmail.com>
2020-02-06 18:59:08 +0100

    Update MInventory - Fix w/ add Player & methods

--- MakiruHUB/src/main/java/net/makiru/api/inventory/MInventory.java
+++ MakiruHUB/src/main/java/net/makiru/api/inventory/MInventory.java
@@ -3,6 +3,7 @@ package net.makiru.api.inventory;
 import net.makiru.api.MItemBuilder;
 import org.bukkit.Bukkit;
 import org.bukkit.Material;
+import org.bukkit.entity.Player;
 import org.bukkit.event.inventory.InventoryType;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.InventoryHolder;
@@ -11,6 +12,7 @@ import org.bukkit.inventory.ItemStack;
 public class MInventory {
 
     private Inventory inventory;
+    private Player player;
 
     public MInventory(int size){
         this(size, "");
@@ -22,6 +24,11 @@ public class MInventory {
 
     public MInventory(InventoryType inventoryType, String title){
         this.inventory = Bukkit.createInventory(null, inventoryType, title);
+    }
+
+    public MInventory(Player player){
+        this.player = player;
+        this.inventory = player.getInventory();
     }
 
     public MInventory(int size, String title, ItemStack... contents){
@@ -71,7 +78,34 @@ public class MInventory {
         return this;
     }
 
+    public MInventory fillWithItem(ItemStack itemStack, int start, int stop){
+        if(this.inventory != null) {
+            for(int i = start; i < stop; i++){
+                inventory.setItem(i, itemStack);
+            }
+            return this;
+        }
+        return this;
+    }
+
+    public MInventory removeAll(ItemStack itemStack){
+        if(this.inventory != null){
+            while(this.inventory.contains(itemStack)){
+                this.inventory.remove(itemStack);
+            }
+            return this;
+        }
+        return this;
+    }
+
     public Inventory asInventory() {
         return this.inventory;
+    }
+
+    public Player getPlayer() {
+        if(player != null || player.isOnline()) {
+            return player;
+        }
+        return Bukkit.getOnlinePlayers().iterator().next();
     }
 }
