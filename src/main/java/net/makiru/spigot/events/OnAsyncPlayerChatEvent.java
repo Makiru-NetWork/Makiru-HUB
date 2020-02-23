8a052921fbee8eafe546329d6a9faebe3e8944e6
Levasseur Wesley <mailpro.levasseur@icloud.com>
2020-02-23 21:51:04 +0100

    Update OnAsyncPlayerChatEvent

--- src/main/java/net/makiru/spigot/events/OnAsyncPlayerChatEvent.java
+++ src/main/java/net/makiru/spigot/events/OnAsyncPlayerChatEvent.java
@@ -3,11 +3,12 @@ package net.makiru.spigot.events;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.AsyncPlayerChatEvent;
+import org.jetbrains.annotations.NotNull;
 
 public class OnAsyncPlayerChatEvent implements Listener {
 
     @EventHandler
-    public void onEvent(AsyncPlayerChatEvent e) {
+    public void onEvent(@NotNull final AsyncPlayerChatEvent e) {
         e.setFormat(e.getPlayer().getDisplayName() + " §8» §f" + e.getMessage());
     }
 }
