e7803a8e1a7d6fc1fb49329e0c87939230bcb21d
Levasseur Wesley <mailpro.levasseur@icloud.com>
2020-02-23 20:00:10 +0100

    Update Items

--- src/main/java/net/makiru/tools/Items.java
+++ src/main/java/net/makiru/tools/Items.java
@@ -8,28 +8,23 @@ import org.bukkit.Material;
 public class Items {
 
     public static ItemStack_B NAVIGATOR_TOOLBAR (Languages language) {
-        String[] text = (String[]) L.NAVIGATOR_TOOLBAR.get(language);
-       return new ItemStack_B(Material.COMPASS).setDisplayName(text[0]).setLore(" ", text[1], " ", text[2]);
+       return new ItemStack_B(Material.COMPASS).setDisplayName((String) L.NAVIGATOR_TOOLBAR.get(language)).setLore((String[]) L.NAVIGATOR_LORES_TOOLBAR.get(language));
     }
 
     public static ItemStack_B ODDS_AND_ENDS_TOOLBAR (Languages language) {
-        String[] text = (String[]) L.ODDS_AND_ENDS_TOOLBAR.get(language);
-        return new ItemStack_B(Material.ENDER_CHEST).setDisplayName(text[0]).setLore(" ", text[1], " ", text[2]);
+        return new ItemStack_B(Material.ENDER_CHEST).setDisplayName((String) L.ODDS_AND_ENDS_TOOLBAR.get(language)).setLore((String[]) L.ODDS_AND_ENDS_LORES_TOOLBAR.get(language));
     }
 
     public static ItemStack_B PROFIL_TOOLBAR (Languages language) {
-        String[] text = (String[]) L.PROFIL_TOOLBAR.get(language);
-        return new ItemStack_B(Material.PLAYER_HEAD).setDisplayName(text[0]).setLore(" ", text[1], " ", text[2]);
+        return new ItemStack_B(Material.PLAYER_HEAD).setDisplayName((String) L.PROFIL_TOOLBAR.get(language)).setLore((String[]) L.PROFIL_LORES_TOOLBAR.get(language));
     }
 
     public static ItemStack_B SHOP_TOOLBAR (Languages language) {
-        String[] text = (String[]) L.SHOP_TOOLBAR.get(language);
-        return new ItemStack_B(Material.EMERALD).setDisplayName(text[0]).setLore(" ", text[1], " ", text[2]);
+        return new ItemStack_B(Material.EMERALD).setDisplayName((String) L.SHOP_TOOLBAR.get(language)).setLore((String[]) L.SHOP_LORES_TOOLBAR.get(language));
     }
 
     public static ItemStack_B SETTINGS_TOOLBAR (Languages language) {
-        String[] text = (String[]) L.SETTINGS_TOOLBAR.get(language);
-        return new ItemStack_B(Material.COMPARATOR).setDisplayName(text[0]).setLore(" ", text[1], " ", text[2]);
+        return new ItemStack_B(Material.COMPARATOR).setDisplayName((String) L.SETTINGS_TOOLBAR.get(language)).setLore((String[]) L.SETTINGS_LORES_TOOLBAR.get(language));
     }
 
    // public static ItemStack_B
