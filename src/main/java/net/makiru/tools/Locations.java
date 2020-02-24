5fae9487087cded423490d7e0fd0223eaa7dbf10
Levasseur Wesley <mailpro.levasseur@icloud.com>
2020-02-24 19:30:32 +0100

    Update Locations.java

--- src/main/java/net/makiru/tools/Locations.java
+++ src/main/java/net/makiru/tools/Locations.java
@@ -7,7 +7,7 @@ import org.jetbrains.annotations.NotNull;
 public enum Locations {
 
     HUB(new Location(MakiruHub.getInstance().getServer().getWorlds().get(0), -539.5, 7, -215.5, 90, 0)),
-    JUMP(new Location(MakiruHub.getInstance().getServer().getWorlds().get(0), 15, 150, 15)),
+    JUMP(new Location(MakiruHub.getInstance().getServer().getWorlds().get(0), -518.5, 23, -253.5, -21, 0)),
     EVENT(new Location(MakiruHub.getInstance().getServer().getWorlds().get(0), -15, 150, -15));
 
     private final Location location;
