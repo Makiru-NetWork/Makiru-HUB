0df14558e2fc369fa4c7be9d774f8cba85900058
Levasseur Wesley <mailpro.levasseur@icloud.com>
2020-03-27 14:39:49 +0100

    Update SettingsGUI.java

--- src/main/java/net/makiru/spigot/guis/SettingsGUI.java
+++ src/main/java/net/makiru/spigot/guis/SettingsGUI.java
@@ -1,8 +1,8 @@
 package net.makiru.spigot.guis;
 
 import net.makiru.commons.Account;
-import net.makiru.spigot.builders.Inventory_B;
-import net.makiru.spigot.lang.L;
+import net.makiru.spigot.api.builders.Inventory_B;
+import net.makiru.spigot.api.tools.L;
 import net.makiru.tools.Items;
 import org.bukkit.entity.Player;
 import org.jetbrains.annotations.NotNull;
@@ -12,7 +12,7 @@ public class SettingsGUI {
     public SettingsGUI(@NotNull final Player player, @NotNull final Account account) {
         player.openInventory(new Inventory_B(player, 9 * 6, (String) L.Hub.SETTINGS.get(account.getLanguage()))
                 .setItem(49, Items.CLOSE_GUI(account.getLanguage()).toItemStack())
-                
+
                 .fillNull(Items.GRAY_DECORATION_GUI().toItemStack(), 0, 8, true)
                 .fillNull(Items.GRAY_DECORATION_GUI().toItemStack(), 45, 53, true)
                 .setItem(9, Items.GRAY_DECORATION_GUI().toItemStack())
