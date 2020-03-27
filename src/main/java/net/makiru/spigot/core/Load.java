03fb2924596f094ceb240d444aaaa6ba73351b86
Levasseur Wesley <mailpro.levasseur@icloud.com>
2020-03-27 14:39:51 +0100

    Update Load.java

--- src/main/java/net/makiru/spigot/core/Load.java
+++ src/main/java/net/makiru/spigot/core/Load.java
@@ -1,11 +1,14 @@
 package net.makiru.spigot.core;
 
+import net.makiru.spigot.api.tools.Tools;
+
 import java.time.ZoneId;
 import java.util.TimeZone;
 
 class Load {
 
-    Load() {
+    Load(MakiruSpigotHub makiruSpigotHub) {
+        Tools.clearCommands(makiruSpigotHub);
         TimeZone.setDefault(TimeZone.getTimeZone(ZoneId.of("Europe/Paris")));
     }
 }
